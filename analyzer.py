40bc86136e2a3b7b56ff44a8c97b212c95ffb50d
Mitch Startzel <mstartzel16@gmail.com>
2019-12-22 18:32:43 -0500

    Falco fram data analysis complete

--- analyzer.py
+++ analyzer.py
@@ -1,34 +1,42 @@
 from slippi import Game
 from slippi.id import InGameCharacter
 from slippi.id import ActionState
+print("test")
+game = Game('MyReplays/Mango1.slp')
+print("test")
+port_number = 2;
+#for i in range (1,3): #this loop gets the port number of the Falco player
+#	if game.frames[0].ports[i] != None and game.frames[0].ports[i].leader.post.character == InGameCharacter.FALCO:
+#		port_number = i
+#		break
 
-game = Game('MyReplays/Test1.slp')
-
-port_number = 0;
-for i in range (0,4): #this loop gets the port number of the Falco player
-	if game.frames[0].ports[1].leader.post.character != InGameCharacter.FALCO:
-		port_number = i
-		break
-
-
+print(port_number)
 back_air_count = 0
 attacking = False
 
 for num, frame in enumerate(game.frames, start=1):
 	data = frame.ports[port_number].leader 
 	post = data.post
-	if post.state == ActionState.ATTACK_AIR_B and not attacking:
+	if post.state == ActionState.ATTACK_AIR_LW and not attacking:
 		attacking = True
 		back_air_count+= 1
-		print("Used Back Air On Frame {}".format(num))
-	elif post.state == ActionState.ATTACK_AIR_B and attacking:
+	elif post.state == ActionState.ATTACK_AIR_LW and attacking:
 		pass
 	else:
 		attacking = False
-	if (num == 1000):
-		print(data)
-		
+	if (num < 300):
+		print("Frame {}".format(num))
+		print(post.state)
+	
+			
 
 test = True
 if test:
-	print(back_air_count)
+	print("Number of Back Airs: {}".format(back_air_count))
+
+
+
+
+
+#NOTES:
+#laser sequence: wait_4, wait_item
